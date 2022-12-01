f33a5dad83ece1675abf0ef1d5b44c09322b821b
Germano Ribeiro <germanno.verde@gmail.com>
2022-12-01 13:08:59 -0300

    Importando mais uma classe de teste

---
+++ VelocidadeMedia.java
@@ -0,0 +1,14 @@
+import Calculadora;
+
+public class VelocidadeMedia{
+    public static void main(String[] args){
+        Calculadora c1 = new Calculadora();
+
+        double distancia = 20;
+
+        double tempo= 10;
+        System.out.println(c1.divisao(distancia,tempo));
+
+    }
+    
+}
