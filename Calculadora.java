afd344adb158b4a319b3e29b9d3c5922ca24377c
Germano Ribeiro <germanno.verde@gmail.com>
2022-12-01 12:41:43 -0300

    Commit class calculadora
    Importei a classe calculadora do meu ambiente de desenvolvimento.

---
+++ Calculadora.java
@@ -0,0 +1,28 @@
+public class Calculadora {
+    public double soma(double n1, double n2){
+        return n1+n2;
+
+    }
+
+    public double subtracao(double n1, double n2){
+        return n1 - n2;
+    
+    }
+    public double divisao(double n1, double n2){
+        return n1 / n2;
+    }    
+
+    public double multiplicacao(double n1, double n2){
+        return n1 * n2;
+    }
+
+    public double raizquadrada (double n1){
+      return  Math.sqrt(n1);
+    }
+    public double exponenciacao(double n1, double n2){
+        return Math.pow(n1,n2);
+    }
+    
+
+}
+
