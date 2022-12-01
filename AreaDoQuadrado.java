bb9ac4941f91dcfb748602ed0e479d0676568f5e
Germano Ribeiro <germanno.verde@gmail.com>
2022-12-01 13:08:01 -0300

    Importando uma das classes de teste

---
+++ AreaDoQuadrado.java
@@ -0,0 +1,11 @@
+import Calculadora;
+
+public class AreaDoQuadrado{
+    public static void main(String[] args){
+        Calculadora c1 = new Calculadora();
+
+        double lado = 4;
+        System.out.println(c1.multiplicacao(lado,lado));
+
+    }
+}
